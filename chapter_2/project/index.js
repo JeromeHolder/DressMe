f0577d6bf952f0a3deaca097c6fcd35f397b965c
Jerome Holder <j.e.holder@outlook.com>
2018-04-11 19:42:00 -0400

    renamed file and removed testing console.log

--- chapter_2/project/index.js
+++ chapter_2/project/index.js
@@ -1,80 +1,70 @@
 const readlineSync = require('readline-sync');
 
-function startGame() {
+function getName() {
     const name = readlineSync.question('What is your name? ');
     console.log("Hello " + name);
-    getMaxFromUser();
 }
 
-startGame();
-
-
-
 function getMaxFromUser() {
-    let number = new Number(readlineSync.question('Choose a number: '));
-    if (isNaN(number)) {
-        console.log("That's not a number!");
-        getMaxFromUser();
-    }
-    else if (number == 0) {
-        console.log("The number cannot be 0.");
-        getMaxFromUser();
-    }
+    let number = Number(readlineSync.questionInt('Choose a number: ', {limitMessage:'That is invalid.'}));
+    return number;
+}
+
+function generateRandomNumber(number) {
+    let max = Math.floor(number);
+    let randomNum = Math.floor(Math.random() * (max));
+    return randomNum;
+}
+
+function getGuessFromUser(randomNum, number) {
+    console.log("Great!  Let's Play!");
+    let guess = Number(readlineSync.questionInt("Guess a number between 0 and " + number + ": ", {limitMessage:'That is invalid.'}));
+    return guess;
+}
+
+function isGuessCorrect(guess, randomNum) {
+    if (guess > randomNum) {
+        console.log("Too high!");
+        return false;
+    } 
+    else if (guess < randomNum) {
+        console.log("Too low!");
+        return false;
+    } 
     else {
-        console.log("Great!  Let's Play!");
-        generateRandomNumber(number);
+        return true;
     }
+}
 
-    function generateRandomNumber(number) {
-        let max = Math.floor(number);
-        let randomNum = Math.floor(Math.random() * (max));
-        getGuessFromUser(randomNum);
+function playAgain(randomNum) {
+    console.log("Correct!  The number was " + randomNum + ".");
+    let playAnswer = readlineSync.keyInYNStrict("Would you like to play again? ");
+    if (playAnswer === true) {
+        return true;
+    } 
+    else {
+        console.log("Thanks for playing.");
+        return false;
+    }
+}
 
-        function getGuessFromUser(randomNum) {
-            let guess = new Number(readlineSync.question("Guess a number between 0 and " + number + ": "));
-            isGuessCorrect(guess);
-
-            function isGuessCorrect(guess) {
-                while (guess !== randomNum) {
-
-                    if (guess > randomNum) {
-                        console.log("Too high!");
-                        guess = new Number(readlineSync.question("Try again: "));
-                    } 
-                    else if (guess < randomNum) {
-                        console.log("Too low!");
-                        guess = new Number(readlineSync.question("Try again: "));
-                    } 
-                    else if (isNaN(guess)) {
-                        console.log("That's not a number!");
-                        getGuessFromUser(randomNum);
-                    }
-                    else {
-                        console.log("Correct!  The number was " + randomNum + ".\nThanks for playing.");
-                        let playAgain = readlineSync.keyInYNStrict("Would you like to play again? ");
-                        if (playAgain === true) {
-                            getMaxFromUser();
-                        }
-                        else {
-                            guess === randomNum;
-                            return;
-                        }
-                        // if (playAgain === 'yes' || playAgain === "Yes" || playAgain === 'y' || playAgain === 'Y') {
-                        //     getMaxFromUser();
-                        // } 
-                        // else if (playAgain === 'no' || playAgain === "No" || playAgain === 'n' || playAgain === 'N') {
-                        //     console.log("Thanks for playing.");
-                        //     guess = randomNum;
-                        //     break;
-                        // }
-                        // else {
-                        //     console.log("I don't understand.  Please enter y, Y, yes or Yes to play again, or n, N, no or No to exit.");
-                        //     playAplayAgain = readlineSync.question("Would you like to play again? ");
-                        //     continue;
-                        // }
-                    }
-                }
+function startGame() {
+    getName();
+    gameLoop();
+    function gameLoop() {
+        let Max = getMaxFromUser();
+        if (Max !== 0) {
+            let rnd = generateRandomNumber(Max);
+            let userGuess = getGuessFromUser(rnd, Max);
+            let guessCorrect = false;
+            while (!isGuessCorrect(userGuess, rnd)) {
+                userGuess = Number(readlineSync.questionInt("Try again: ", {limitMessage:'That is invalid.'}));
+            }
+            if (playAgain(rnd)) {
+                gameLoop();
             }
         }
     }
 }
+
+startGame();
