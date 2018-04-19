230a10c3e5fa8bd387ed01591b627798303795ea
Jerome Holder <j.e.holder@outlook.com>
2018-04-19 15:58:51 -0400

    Spoiler Machine complete

--- chapter_3/project/spoiler_machine/spoiler.js
+++ chapter_3/project/spoiler_machine/spoiler.js
@@ -1,97 +1,62 @@
 const request = require("request");
 const cheerio = require("cheerio");
 
-// let movie;
-// let warningTime;
-
-// if(!isNaN(parseInt(process.argv[2]))) {  
-//     movie = process.argv[3];
-//     warningTime = parseInt(process.argv[2]);
-//     // console.log(movie + warningTime);
-// }
-// else {
-//     movie = process.argv[2];
-//     warningTime = parseInt(process.argv[3]);
-//     // console.log(movie + warningTime);
-// }
-
-// const url = "https://www.google.ca/search?q=" + movie + "movie" + "news" + "-imdb";
-// if (warningTime < 0) {
-//     console.log('Error: Warning time cannot be negative');
-//     // return;
-// }
-// else if (process.argv.length < 4) {
-//     console.log('Error: Not enough arguments.  Enter movie title (Surrounded by quotes if title is more than one word) and warning time.');
-// }
-// else if (process.argv.length > 4) {
-//     console.log('Error: Too many arguments.  Enter movie title (Surrounded by quotes if title is more than one word) and warning time.');
-// }
-// else {
-//     console.log("\n**spoiler warning** about to spoil the movie " + movie + " in " + warningTime + " seconds\n");
-//     request(url, function(error, response, body) {
-//         // let totalResults = 0;
-//         if(error) {
-//             console.log("Couldn't get page because of error: " + error);
-//             return;
-//         }
-    
-//         var $ = cheerio.load(body),
-//             hline = $(".r a");
-        
-//         hline.each(function (i, hline) {
-//             var title = $(hline).text();
-//             console.log(title);
-//         })
-//     })
-    
-//     setTimeout(function(){
-//         console.log("\nThis is the spoiler!");
-//     }, (warningTime * 1000));
-// }
+let movie;
+let warningTime;
+let title = [];
+let headlines;
+let overview;
 
 
+// Accept command line arguments in any order and assigning the variables accordingly
+if(!isNaN(parseInt(process.argv[2]))) {  
+    movie = process.argv[3];
+    warningTime = parseInt(process.argv[2]);
+}
+else {
+    movie = process.argv[2];
+    warningTime = parseInt(process.argv[3]);
+}
+const url = "https://www.google.ca/search?q=" + movie + "movie" + "news" + " -imdb";
+const tmdbURL = 'https://api.themoviedb.org/3/search/movie?include_adult=false&page=1&language=en-US&api_key=1f39c18e7001daed72e0a151db90566a&query=' + movie;
 
-// with http
-// var http = require("https");
-
-// var options = {
-//   "method": "GET",
-//   "hostname": "api.themoviedb.org",
-//   "port": null,
-//   "path": "/3/search/movie?include_adult=false&page=1&query=arrival&language=en-US&api_key=1f39c18e7001daed72e0a151db90566a",
-//   "headers": {}
-// };
-
-// var req = http.request(options, function (res) {
-//   var chunks = [];
-
-//   res.on("data", function (chunk) {
-//     chunks.push(chunk);
-//   });
-
-//   res.on("end", function () {
-//     var body = Buffer.concat(chunks);
-//     let obj = JSON.parse(body);
-//     console.log(obj.results[0].overview);
-//   });
-// });
-
-// req.write("{}");
-// req.end();
-
-// with request
-
-var options = { method: 'GET',
-  url: 'https://api.themoviedb.org/3/search/movie',
-  qs: 
-   { include_adult: 'false',
-     page: '1',
-     language: 'en-US',
-     api_key: '<<api_key>>' },
-  body: '{}' };
-
-request(options, function (error, response, body) {
+// // Test if the movie exists, stores spoiler and headlines if it does
+request(tmdbURL, function (error, response, body) {
+  let obj = JSON.parse(body);
   if (error) throw new Error(error);
-
-  console.log(body);
+  else if (obj.results.length === 0) {
+    console.log("That movie doesn't exist");
+    return;
+  }
+  else {
+    overview = "\n" + obj.results[0].overview + "\n";
+    // Testing number and value of command line arguments
+    if (warningTime < 0) {
+      console.log('Error: Warning time cannot be negative');
+    }
+    else if (process.argv.length < 4) {
+      console.log('Error: Not enough arguments.  Enter movie title (Surrounded by quotes if title is more than one word) and warning time.');
+    }
+    else if (process.argv.length > 4) {
+      console.log('Error: Too many arguments.  Enter movie title (Surrounded by quotes if title is more than one word) and warning time.');
+    }
+    else {
+      console.log("\n**spoiler warning** about to spoil the movie " + movie + " in " + warningTime + " seconds\n");
+      setTimeout(function(){
+        console.log(overview);
+      }, (warningTime * 1000));
+      request(url, function(error, response, body) {
+        if(error) {
+            console.log("Couldn't get page because of error: " + error);
+            return;
+        }
+        var $ = cheerio.load(body), hline = $(".r a");
+        hline.each(function (i, hline) {
+            title.push($(hline).text()+"\n");
+        })
+        headlines = title.join("");
+        console.log(headlines);
+      })
+    }
+  }
 });
