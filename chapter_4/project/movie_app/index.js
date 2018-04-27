3a7bd8c51d357587072bf6e3fa2dfe2d9f5dcb04
Jerome Holder <j.e.holder@outlook.com>
2018-04-26 20:25:55 -0400

    Functionality and Styling Complete --iframe unresponsive

--- chapter_4/project/movie_app/index.js
+++ chapter_4/project/movie_app/index.js
@@ -4,6 +4,7 @@ const request = require("request");
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
+// Calls API for popular movies
 let popurl = 'https://api.themoviedb.org/3/movie/popular?api_key=1f39c18e7001daed72e0a151db90566a&language=en-US&page=1'
 let popresults = {};
 request(popurl, (error, res, body) => {
@@ -16,8 +17,10 @@ request(popurl, (error, res, body) => {
     }
 });
 
+// Calls API for specific movie >> gets movie ID from dynamic link in search.ejs and index.ejs
 app.get('/movie/:movieId', (req, res) => {
-    let movieurl = 'https://api.themoviedb.org/3/movie/' + req.params.movieId + '?api_key=1f39c18e7001daed72e0a151db90566a&language=en-US';
+    // Used append_to_response to include /videos for trailer link
+    let movieurl = 'https://api.themoviedb.org/3/movie/' + req.params.movieId + '?api_key=1f39c18e7001daed72e0a151db90566a&language=en-US&append_to_response=videos';
     request(movieurl, (err, response, content) => {
         if(err) {
             console.log(err);
@@ -28,6 +31,7 @@ app.get('/movie/:movieId', (req, res) => {
     })
 });
 
+// Calls API for search terms
 app.get('/search', (req, res) => {
     let searchURL = "https://api.themoviedb.org/3/search/movie?api_key=1f39c18e7001daed72e0a151db90566a&language=en-US&query=" + req.query.searchTerm + "&page=1&include_adult=false"
     request(searchURL, (err, response, data) => {
@@ -41,8 +45,10 @@ app.get('/search', (req, res) => {
     })
 })
 
+// Redirects any non-existent endpoint to index
 app.get('*', (req, res) =>{
     res.render('index', popresults);
 })
+
 
 app.listen(8080, () => {console.log('Server started on http://localhost:8080\nPress CTRL + C to stop server');});
