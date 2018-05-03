46fcb4e820931ff4dc4a636ab7bf87b96ba66353
Jerome Holder <j.e.holder@outlook.com>
2018-05-03 10:08:26 -0400

    Updated wk 5 state exercise

--- chapter_5/exercises/react_state/src/App.js
+++ chapter_5/exercises/react_state/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './App.css'
 /* List of image urls ------------------------------
 'http://imgur.com/9itd49u.png'
 'http://imgur.com/n19BXfZ.png'
@@ -7,17 +8,39 @@ import React, { Component } from 'react';
 -------------------------------------------------- */
 
 class App extends Component {
+	constructor(){
+		super();
+		this.state={
+			index:0
+		}
+		this.nextSlide = this.nextSlide.bind(this);
+		this.previousSlide = this.previousSlide.bind(this);
+	}
+
+	nextSlide(){
+		this.setState({index: this.state.index + 1})
+	}
+
+	previousSlide(){
+		this.setState({index: this.state.index -1})
+	}
+
   render() {
+
+	const myImages = this.props.imageUrls.map((image, index)=>{
+		return <img src={image} className={index===this.state.index ? '' : 'hidden'} />
+	})
 		return (
 			<div className="App">
 				<h1 >Calvin Carousel</h1>
 			    <div>
-			    	<button onClick>Previous</button>
-			      	<span>1 of 4</span>
-			      	<button onClick>Next</button>
+			    	<button disabled={this.state.index === 0 ? true : false} onClick={this.previousSlide}>Previous</button>
+			      	<span>{this.state.index + 1} of 4</span>
+			      	<button disabled={this.state.index === this.props.imageUrls.length-1 ? true : false} onClick={this.nextSlide}>Next</button>
 			    </div>
 				<div>
-					<img src="http://imgur.com/9itd49u.png" />
+					{myImages}
+
 				</div>
 			</div>			
 		)
--- chapter_5/exercises/react_state/src/index.js
+++ chapter_5/exercises/react_state/src/index.js
@@ -4,5 +4,12 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const urls= [
+    'http://imgur.com/9itd49u.png',
+    'http://imgur.com/n19BXfZ.png',
+    'http://imgur.com/VBwQmzA.png',
+    'http://imgur.com/nawDxVv.png'
+]
+
+ReactDOM.render(<App imageUrls={urls}/>, document.getElementById('root'));
 registerServiceWorker();
