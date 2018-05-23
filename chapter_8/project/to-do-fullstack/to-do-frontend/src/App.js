5355e58a1691c58fba489401c4442cdc9e987298
Jerome Holder <j.e.holder@outlook.com>
2018-05-23 12:28:22 -0400

    wk8 proj diving deeper complete - diving even deeper not yet

---
+++ chapter_8/project/to-do-fullstack/to-do-frontend/src/App.js
@@ -0,0 +1,138 @@
+import React, { Component } from 'react';
+import './App.css';
+import TodoList from './TodoList';
+import TodoForm from './TodoForm';
+import SelectDropdownFilter from './SelectDropdownFilter';
+import Counter from './Counter';
+import axios from 'axios';
+
+export default class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      todos: [],
+      // Default state to have the clear complete button disabled
+      clearButtonDisabled: true,
+      // Default state for the filter
+      filter: 'all'
+    };
+  };
+
+  // Gets existing Todos from the db
+  componentDidMount(){
+    axios.get('http://localhost:8080/todos')
+         .then(result =>{
+           this.setState({
+             todos: result.data
+           });
+         })
+         .catch(err=>{
+           console.log(err);
+         });
+  };
+
+  // onChange handler that changes state needed for the filter conditions in render()
+  setFilter = selected => {
+    this.setState({
+      filter: selected.target.value
+    });
+  };
+
+  // Tests the presence of completed items and changes state accordingly
+  checkClearState = () => {
+    if(this.state.todos.find(i => i.complete === true)){
+      this.setState({
+        clearButtonDisabled: false
+      });
+    }
+    else {
+      this.setState({
+        clearButtonDisabled: true
+      });
+    };
+  };
+
+  // onChange handler called from Todo
+  setComplete = (i,c, con) => {
+    let updateTodo = {
+      id:i,
+      complete: !c,
+      content:con
+    }
+    // Filter out the one to be edited
+    let copy = this.state.todos.filter((todo) => {return todo.id !== i})
+    axios.put('http://localhost:8080/todos', updateTodo)
+         .then(result => {
+            //  Push the result into the filtered copy and use it to overwrite state
+            copy.push(result.data);
+            this.setState({
+              todos: copy
+            }, ()=>this.checkClearState());
+         })
+         .catch(err =>{
+           console.log(err);
+         });
+  };
+
+  // Called from TodoForm to add new items to the list of todos in state
+  addTodo = content => {
+    let newTodo = {
+      content: content,
+      complete: false
+    };
+    let copy = Array.from(this.state.todos);
+    axios.post('http://localhost:8080/todos', newTodo)
+         .then(result =>{
+          copy.push(result.data);
+          this.setState({
+            todos: copy
+          });
+         });
+  };
+
+  // Click handler for the Clear Complete button
+  clearComplete = e => {
+    e.preventDefault();
+    axios.delete('http://localhost:8080/todos')
+         .then(result => {
+          let copy = this.state.todos.filter((copyTodos) => {
+            return copyTodos.complete === false;
+          });
+          this.setState({
+            todos: copy,
+            // Resets the Clear Complete button to disabled
+            clearButtonDisabled: true
+          });
+         });
+  };
+
+  render() {
+    // Filters the list of todos in state and returns a new array without altering state
+    // New array is passed to TodoList
+    let filteredListJSX = [];
+    if (this.state.filter === 'active'){
+      filteredListJSX = this.state.todos.filter((filteredTodos) => {
+        return filteredTodos.complete === false
+      });
+    }
+    else if (this.state.filter === 'complete'){
+      filteredListJSX = this.state.todos.filter((filteredTodos) => {
+        return filteredTodos.complete === true
+      });
+    }
+    else {
+      filteredListJSX = this.state.todos
+    };
+
+    return (
+      <div className="container">
+        <h1 className="text-center titleColor">My To-Do List</h1>
+        <Counter listContent={this.state.todos} />
+        <TodoForm addTodo={this.addTodo}/>
+        <TodoList listContent={filteredListJSX} setComplete={this.setComplete}/>      
+        <SelectDropdownFilter setFilter={this.setFilter}/>
+        <button onClick={this.clearComplete} disabled={this.state.clearButtonDisabled} className="pull-right btn btn-primary btnColor">Clear Complete</button>       
+      </div>
+    );
+  };
+};
---
+++ chapter_8/project/to-do-fullstack/to-do-frontend/src/Todo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Todo extends React.Component {
+    render() {
+        return (
+            <li className="list-group-item">
+            {/* onChange handler that toggles completed status by applying the 'done' class to the label */}
+                <input type="checkbox" checked={this.props.complete} onChange={()=>{this.props.setComplete(this.props.id, this.props.complete, this.props.content)}}/>
+                <label className={ (this.props.complete) ? 'done':'' }>{this.props.content}</label>
+            </li>
+        );
+    }
+}
